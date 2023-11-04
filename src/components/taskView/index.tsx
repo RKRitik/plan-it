84d09a436c777d89c8798ad57e5c3a303f6419c2
ritik <khatri.ritik16@gmail.com>
2023-11-04 19:42:17 +0530

    setup all task page and add task page

--- src/components/taskView/index.tsx
+++ src/components/taskView/index.tsx
@@ -1,11 +1,64 @@
-import { Button, Text, View } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
+import { useForm, Controller, SubmitErrorHandler } from 'react-hook-form';
+
+type Inputs = {
+  example: string;
+  description: string;
+};
 
 type Props = { route: any; navigation: any };
 export default function TaskView({ route, navigation }: Props) {
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit = (data: Inputs) => {
+    console.log(data);
+  };
+
+  const onError: SubmitErrorHandler<Inputs> = (errors, e) => {
+    return console.log(errors);
+  };
+
+  function inputController(inputData: any) {
+    return (
+      <Controller
+        control={control}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <View className="flex flex-row items-center gap-4 mb-4">
+            <Text className="font-medium ">{inputData.label}</Text>
+            <TextInput
+              placeholder={`Enter ${inputData.name}`}
+              className="bg-slate-300 w-full border border-slate-300 rounded-sm"
+              onBlur={onBlur}
+              onChangeText={value => onChange(value)}
+              value={value}
+            />
+          </View>
+        )}
+        name={inputData.name}
+        rules={{ required: inputData.required }}
+      />
+    );
+  }
+
   return (
-    <View>
-      <Text>Add/Edit Task</Text>
-      <Button onPress={() => navigation.goBack()} title="back ?"></Button>
+    <View className="relative h-full ">
+      <View className="px-4">
+        {inputController({ name: 'name', label: 'Name' })}
+        {inputController({
+          name: 'description',
+          required: true,
+          label: 'Description',
+        })}
+      </View>
+      <View className="absolute bottom-0 w-full">
+        <Button onPress={handleSubmit(onSubmit, onError)} title="Save"></Button>
+      </View>
     </View>
   );
 }
--- src/components/tasks/index.tsx
+++ src/components/tasks/index.tsx
@@ -1,15 +1,35 @@
-import { Button, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { TASKS } from './data';
 
 type Props = { route: any; navigation: any };
 
-export default function Tasks({ route, navigation }: any) {
+function task(taskData: any) {
   return (
-    <View>
-      <Text className="text-black text-[22px]">Tasks</Text>
-      <Button
-        title="Add Task"
-        onPress={() => navigation.navigate('TaskView')}
-      />
+    <TouchableOpacity key={taskData.id}>
+      <View className="bg-slate-400 p-4 mb-4 rounded-lg">
+        <Text>{taskData.name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+export default function Tasks({ route, navigation }: Props) {
+  const [tasks, setTasks] = useState(TASKS);
+
+  return (
+    <View className="relative h-full ">
+      <ScrollView className="mb-[16px] px-4">
+        {tasks.map((task_: any) => {
+          return task(task_);
+        })}
+      </ScrollView>
+      <View className="absolute bottom-0 w-full">
+        <Button
+          title="Add Task"
+          onPress={() => navigation.navigate('TaskView')}
+        />
+      </View>
     </View>
   );
 }
